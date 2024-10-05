d77f7e096cc1505046d2b3b39edb1d620bbf5ee9
mohamedvpn <103552750+mohamedvpn@users.noreply.github.com>
2024-10-06 02:45:40 +0300

    Update insta_Activation.py

--- insta_Activation.py
+++ insta_Activation.py
@@ -3,14 +3,11 @@
     "sessionid": "2034060630%3AoiVF5esx83aYzI%3A19%3AAYcVV0H6ff1Vb_o6mvF6iHZpO_5-h7_x-l0A5p-Pqg",
     "csrftoken": "Iloy5N3Q0tNeHLCGg07ayZbaRoo3ersL",
     "ds_user_id": "2034060630",
-    "Deyaa":"",
-    "Saieed":"",
-    "نور الدين","",
-    "Ahmed-Khaled":"",
-    "Fares-K-Bakr":"",
-    "Tarek-Mohamed":"",
-    "Ahmed-Khaled":"",
-    "Ahmed-Khaled":"",
-    "Ahmed-Khaled":"",
-    
+    "Deyaa":"1",
+    "Saieed":"1",
+    "نور الدين","1",
+    "Ahmed-Khaled":"1",
+    "Fares-K-Bakr":"1",
+    "Tarek-Mohamed":"1",
+
 }
